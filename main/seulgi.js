6139ee7572a99ed2890b513e8a3890fabbdc6581
HMQ <hmquan191@gmail.com>
2025-02-20 18:15:23 +0700

    update wendy seulgi

--- main/seulgi.js
+++ main/seulgi.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
   const subTitle = document.querySelector(".sub-title-main");
   const infoTexts = [
-    "Kang Seul-gi, known mononymously as Seulgi, is a South Korean singer and dancer. She is a member of the South Korean girl group Red Velvet, its sub-unit Red Velvet – Irene & Seulgi, and the supergroup Got the Beat. She released her debut EP, 28 Reasons, in 2022.",
+    "Kang Seul-gi, known as Seulgi, is a South Korean singer. She is a member of Red Velvet, sub-unit Red Velvet – Irene & Seulgi, and the supergroup Got the Beat. She released her debut EP, 28 Reasons, in 2022.",
     "She was a part of the pre-debut team SM Rookies and was the first member to be revealed.<br>Her representative fruit: Pineapple <br>Her representative weapon: Knife",
     "She was casted in 2007 through public audition. <br>Her hobbies are drawing and playing the guitar.<br>Her designated color is yellow/tangerine.",
     "On October 4, 2022 she debuted as a soloist with the mini album 28 Reasons.<br>Seulgi will return with her second solo Accidentally On Purpose on March 10, 2025.",
